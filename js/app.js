8c8df9847a4ec817ccd8321c8bc578e7ac26643b
Jake Bell <jakebrbell@gmail.com>
2016-11-29 09:40:28 -0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,10 +7,10 @@
     $('#listings').empty();
 
     for (const movie of movies) {
-      const $col = $('<div class="col s6">');
-      const $card = $('<div class="card hoverable">');
-      const $content = $('<div class="card-content center">');
-      const $title = $('<h6 class="card-title truncate">');
+      const $col = $('<div>').addClass('col s6');
+      const $card = $('<div>').addClass('card hoverable');
+      const $content = $('<div>').addClass('card-content center');
+      const $title = $('<h6>').addClass('card-title truncate');
 
       $title.attr({
         'data-position': 'top',
@@ -20,7 +20,7 @@
       $title.tooltip({ delay: 50, });
       $title.text(movie.title);
 
-      const $poster = $('<img class="poster">');
+      const $poster = $('<img>').addClass('poster');
 
       $poster.attr({
         src: movie.poster,
@@ -30,21 +30,23 @@
       $content.append($title, $poster);
       $card.append($content);
 
-      const $action = $('<div class="card-action center">');
-      const $plot = $('<a class="waves-effect waves-light btn modal-trigger">');
+      const $action = $('<div>').addClass('card-action center');
+      const $plot = $('<a>');
 
+      $plot.addClass('waves-effect waves-light btn modal-trigger');
       $plot.attr('href', `#${movie.id}`);
       $plot.text('Plot Synopsis');
 
       $action.append($plot);
       $card.append($action);
 
-      const $modal = $(`<div id="${movie.id}" class="modal">`);
-      const $modalContent = $('<div class="modal-content">');
+      const $modal = $('<div>').addClass('modal');
+      const $modalContent = $('<div>').addClass('modal-content');
       const $modalHeader = $('<h4>').text(movie.title);
       const $movieYear = $('<h6>').text(`Released in ${movie.year}`);
       const $modalText = $('<p>').text(movie.plot);
 
+      $modal.attr('id', movie.id)
       $modalContent.append($modalHeader, $movieYear, $modalText);
       $modal.append($modalContent);
 
